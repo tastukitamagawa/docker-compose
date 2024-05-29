836fd514cb09b9d141af0c126478f346c8f204a4
Tatsuki Tamagawa <tasuiqi27@gmail.com>
2024-05-30 07:07:59 +0900

    index.phpの作成

---
+++ app/src/index.php
@@ -0,0 +1,15 @@
+<?php
+    // 接続
+    $dsn = 'mysql:dbname=test_db;host=run-php-db;';
+    $user = 'test';
+    $password = 'test';
+
+    try {
+        $pdo = new PDO($dsn, $user, $password);
+        $sth = $pdo->query("SELECT * FROM users WHERE id = 1");
+        $user = $sth->fetch(PDO::FETCH_ASSOC);
+        var_dump($user);
+    } catch (PDOException $e) {
+        print('Error:'.$e->getMessage());
+        exit;
+    }
